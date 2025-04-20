d3d730074ba28b78d33d0aea9174715ee96f15c1
TheLongIslander <157660154+TheLongIslander@users.noreply.github.com>
2025-04-20 04:11:29 -0400

    Added time recorded to main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "io.h"
 #include "nbt_parser.h"
 #include "nbt_utils.h"
-
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -19,7 +19,18 @@ int main(int argc, char* argv[]) {
     }
 
     size_t offset = 0;
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     parse_nbt(data, &offset, 0);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    double elapsed_ms = (end.tv_sec - start.tv_sec) * 1000.0 +
+                        (end.tv_nsec - start.tv_nsec) / 1e6;
+
+    printf("Parsed and printed in %.2f ms\n", elapsed_ms);
 
     free(data);
     return 0;
